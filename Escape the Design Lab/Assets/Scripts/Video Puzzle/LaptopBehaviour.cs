d5588fb5aadf40ee8f3b5fe8410b2a0e0d10ccba
R4HK00N <robert.eddy.beltman@gmail.com>
2025-06-18 15:13:09 +0200

    Revert "video editor fix (poging)"
    This reverts commit 595f9c14502ebc89038a81df49db45acc4478162.

--- Escape the Design Lab/Assets/Scripts/Video Puzzle/LaptopBehaviour.cs
+++ Escape the Design Lab/Assets/Scripts/Video Puzzle/LaptopBehaviour.cs
@@ -50,7 +50,7 @@ public class LaptopBehaviour : MonoBehaviour
         }
         else
         {
-            laptopHintText.text = "Vind " + usbToCollect + " USB Sticks";
+            laptopHintText.text = "Vindt " + usbToCollect + " USB Sticks";
 
             usbInLaptop[usbToCollect].SetActive(true);
         }
