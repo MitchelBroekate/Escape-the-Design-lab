19b9e11a5794eeb449c90bcc2d612b232ddd96f1
MitchelBroekate <mitchelb11@outlook.com>
2025-06-18 14:24:02 +0200

    Timer multiple timer fix

--- Escape the Design Lab/Assets/Scripts/Timer.cs
+++ Escape the Design Lab/Assets/Scripts/Timer.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 using TMPro;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Timer : MonoBehaviour
 {
-    [SerializeField] float timerDuration = 60.0f; // Duration in seconds, adjustable in Inspector
-    [SerializeField] TMP_Text timerText; // Reference to the TMP text object, assign in Inspector
+    [SerializeField] float timerDuration = 60.0f; // Duration in seconds, adjustable in Inspector // Reference to the TMP text object, assign in Inspector
+    [SerializeField] List<TMP_Text> timersText = new();
 
     public bool gameWon;
     public bool startTimer = false;
@@ -40,19 +41,22 @@ public class Timer : MonoBehaviour
 
     void UpdateTimerDisplay(float time)
     {
-        if (timerText != null)
-        {
             if (time > 0)
             {
                 int minutes = Mathf.FloorToInt(time / 60);
                 int seconds = Mathf.FloorToInt(time % 60);
-                timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+                foreach (TMP_Text text in timersText)
+                {
+                    text.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+                }
             }
             else
             {
-                timerText.text = "Time's Up!";
+                foreach (TMP_Text text in timersText)
+                {
+                    text.text = "Time's Up!";
+                }
             }
-        }
     }
 
     IEnumerator GameOver()
